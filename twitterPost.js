827bf5950bd5b5d3cba77129d02deb2a847ef089
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 16:21:29 +0530

    twitter post objects literals

---
+++ twitterPost.js
@@ -0,0 +1,10 @@
+//create a object Literal for properties of twiter post
+
+let properties={
+    username:"__shubham__07",
+    content:"Challlenge excepted for 100 days ",
+    likes:50,
+    repost:23,
+    tags:"@pavan ranajne ,@ashish ranjane"
+}
+console.log(properties);
