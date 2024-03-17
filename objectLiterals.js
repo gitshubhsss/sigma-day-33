687f7b6e2e14af126d6edf1832886ee4d3b1eb2c
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 16:16:19 +0530

    objects literals in js

---
+++ objectLiterals.js
@@ -0,0 +1,8 @@
+let student={
+    name:"shubham ranajne",
+    class:"Fy MSc computer science",
+    roll:4044,
+    email:"shubhamranjane16@gmail.com"
+}
+console.log(student);
+console.log(typeof student);
