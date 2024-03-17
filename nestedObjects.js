5ea96a4764cfc6ea5d32bfb16eefc7cb0b9694d9
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 16:48:00 +0530

    nested Objects Objects in Objects

---
+++ nestedObjects.js
@@ -0,0 +1,16 @@
+const classInfo={
+    aman:{
+        grade:"A+",
+        city:"Delhi"
+    },
+    shraddhaDi:{
+        grade:"A",
+        city:"pune"
+    },
+    Saumya:{
+        grade:"B+",
+        city:"mumbai"
+    }
+    
+}
+console.log(classInfo.aman.grade);
