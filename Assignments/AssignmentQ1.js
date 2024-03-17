710fd3bc798b657ba0c6795ee4484d00e5483fde
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 18:57:31 +0530

    Assigment 1

---
+++ Assignments/AssignmentQ1.js
@@ -0,0 +1,6 @@
+//create a program that generates a random number representing  a dice roll 
+//the numbers should be inbeetween 1 to 6
+
+let randomNum=Math.random()*6;
+let exactNum=Math.floor(Math.random()*6 +1);
+console.log(exactNum);
