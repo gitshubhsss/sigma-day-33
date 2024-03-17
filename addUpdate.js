84be035a3cbec0736cf95f2abd2b53889d0fd608
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 16:42:01 +0530

    javaScript objects

---
+++ addUpdate.js
@@ -0,0 +1,10 @@
+let student={
+    name:"shubham ranajne",
+    age:21,
+    marks:84,
+    city:"pune"
+}
+student.city="mumbai";//here we have changed the city
+student.gender="male";//for adding new key value pairs in the object
+delete student.age;//it will delete the key and values from the object
+console.log(student);
