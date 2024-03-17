b94963d71865d6568664ebcd5c45e4cf810a5af2
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 16:53:40 +0530

    Arrays of objects

---
+++ ArrayOfObjects.js
@@ -0,0 +1,19 @@
+const Class=[
+    {
+        name:"shubham",
+        grade:"A+",
+        city:"pune"
+    },
+    {
+        name:"omkar",
+        grade:"A+",
+        city:"Delhi"
+    },
+    {
+        name:"shraddha",
+        grade:"A+",
+        city:"jaipur"
+    },
+
+];
+console.log(Class[0].name);
