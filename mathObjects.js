994b7aad671156551ca751f3f4b0a7f14e82eda8
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 17:42:00 +0530

    math funtion in javascript

---
+++ mathObjects.js
@@ -0,0 +1,9 @@
+//create a random numbers from 1 to 100;
+// let randomNum=Math.random()*100;
+// let ExactNum=Math.floor(randomNum+1)
+// console.log(ExactNum);
+
+//supporsingly i wants to print the numbers form 21 to 25;
+randomNum=Math.random()*5+21;
+ExactNum=Math.floor(randomNum);
+console.log(ExactNum);
