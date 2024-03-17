3f0a0da2cc33dd151629b73c1e9184a0d35fa4bf
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 19:03:04 +0530

    Assigment 2

---
+++ Assignments/AssignmentQ2.js
@@ -0,0 +1,9 @@
+//Create an object representing a car that stores the following properties for the car:name ,model color
+//print the car name
+
+let car={
+    name:"lamborghini",
+    price:" 3.22 Crore",
+    color:"orange"
+}
+console.log(car.name);
