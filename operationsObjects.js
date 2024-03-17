ca4160b803751452e2da3ee28a1010292d99c5f0
gitshubhsss <shubhamranjane16@gmail.com>
2024-03-17 16:34:17 +0530

    javaScript converts the key into the strings

---
+++ operationsObjects.js
@@ -0,0 +1,17 @@
+let obj={
+    1:'a',
+    2:'b',
+    undefined:"nothing",
+    null:"null value",
+    true:"yeps"
+}
+console.log(obj.true);
+
+/* output :{
+    '1': 'a',
+    '2': 'b',
+    undefined: 'nothing',
+    null: 'null value',
+    true: 'yeps'
+    //javascript will convert all the keys to the strings 
+ }*/
